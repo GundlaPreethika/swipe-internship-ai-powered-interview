f8ce1d0f55945994a81c6dc290bde2442b569f33
GundlaPreethika <119915020+GundlaPreethika@users.noreply.github.com>
2025-09-28 00:13:41 +0530

    Update api.js

---
+++ swipe-interview/src/utils/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL:
+    process.env.NODE_ENV === 'production' ? '' : 'http://localhost:4000',
+  timeout: 30000,
+});
+
+export default api;
